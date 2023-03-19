364bdb967337e7cf50e939314ef4374b0aa0fe13
Issam Robler <samydob@gmail.com>
2023-03-19 09:23:41 -0400

    Made experience page

--- src/pages/experience/Experience.tsx
+++ src/pages/experience/Experience.tsx
@@ -2,12 +2,21 @@ import { TFunction } from "i18next";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import {
+  AzureIcon,
+  CSharpIcon,
+  ExpressIcon,
+  GitHubIcon,
   JavaScriptIcon,
+  MaterialUIIcon,
+  MochaIcon,
   MongoDBIcon,
+  NodeIcon,
   PostgreSQLIcon,
   PythonIcon,
   ReactIcon,
   TypeScriptIcon,
+  VisualStudioCodeIcon,
+  VisualStudioIcon,
 } from "../../assets/dev-icons/dev-icons";
 import { InfoPaper } from "../../components/paper/Paper";
 import { Text, Title } from "../../components/Text/Text";
@@ -16,58 +25,106 @@ interface IPaperContentProps {
   t: TFunction;
 }
 
-const PassionContent = ({ t }: IPaperContentProps) => (
-  <ul>
-    <li>
-      <Text>{t("about.passions.tech")}</Text>
-    </li>
-    <li>
-      <Text>{t("about.passions.videogames")}</Text>
-    </li>
-    <li>
-      <Text>{t("about.passions.sports")}</Text>
-    </li>
-  </ul>
-);
-
-const CareerInterestContent = ({ t }: IPaperContentProps) => (
-  <ul>
-    <li>
-      <Text>{t("about.careerInterest.five")}</Text>
-    </li>
-    <li>
-      <Text>{t("about.careerInterest.ten")}</Text>
-    </li>
-    <li>
-      <Text>{t("about.careerInterest.twenty")}</Text>
-    </li>
-  </ul>
-);
-
-const LearningContent = () => (
+const MacacadiamProgrammingList = () => (
   <ProgrammingLanguageList>
-    <ReactIcon />
     <TypeScriptIcon />
     <JavaScriptIcon />
-    <PythonIcon />
-    <PostgreSQLIcon />
-    <MongoDBIcon />
+    <ReactIcon />
+    <MaterialUIIcon />
+    <GitHubIcon />
+    <VisualStudioCodeIcon />
   </ProgrammingLanguageList>
+);
+const MacadamianContent = ({ t }: IPaperContentProps) => (
+  <>
+    <MacacadiamProgrammingList />
+    <ul>
+      <li>
+        <Text>{t("experience.macadamian.details.d1")}</Text>
+      </li>
+      <li>
+        <Text>{t("experience.macadamian.details.d2")}</Text>
+      </li>
+      <li>
+        <Text>{t("experience.macadamian.details.d3")}</Text>
+      </li>
+    </ul>
+  </>
+);
+
+const CartoVistaProgrammingList = () => (
+  <ProgrammingLanguageList>
+    <TypeScriptIcon />
+    <CSharpIcon />
+    <ReactIcon />
+    <PostgreSQLIcon />
+    <GitHubIcon />
+    <VisualStudioCodeIcon />
+    <VisualStudioIcon />
+  </ProgrammingLanguageList>
+);
+const CartoVistaContent = ({ t }: IPaperContentProps) => (
+  <>
+    <CartoVistaProgrammingList />
+    <ul>
+      <li>
+        <Text>{t("experience.cartovista.details.d1")}</Text>
+      </li>
+      <li>
+        <Text>{t("experience.cartovista.details.d2")}</Text>
+      </li>
+      <li>
+        <Text>{t("experience.cartovista.details.d3")}</Text>
+      </li>
+      <li>
+        <Text>{t("experience.cartovista.details.d4")}</Text>
+      </li>
+    </ul>
+  </>
+);
+
+const ESDCProgrammingList = () => (
+  <ProgrammingLanguageList>
+    <TypeScriptIcon />
+    <NodeIcon />
+    <ExpressIcon />
+    <VisualStudioCodeIcon />
+    <AzureIcon />
+    <GitHubIcon />
+    <MochaIcon />
+  </ProgrammingLanguageList>
+);
+
+const ESDCContent = ({ t }: IPaperContentProps) => (
+  <>
+    <ESDCProgrammingList />
+    <ul>
+      <li>
+        <Text>{t("experience.esdc.details.d1")}</Text>
+      </li>
+      <li>
+        <Text>{t("experience.esdc.details.d2")}</Text>
+      </li>
+      <li>
+        <Text>{t("experience.esdc.details.d3")}</Text>
+      </li>
+    </ul>
+  </>
 );
 
 export default function Experience() {
   const { t } = useTranslation();
   return (
     <>
-      <Title>{t("about.title")}</Title>
-      <InfoPaper title={t("about.passions.subtitle")}>
-        <PassionContent t={t} />
+      <Title>{t("experience.title")}</Title>
+      <InfoPaper title={t("experience.esdc.subtitle")}>
+        <ESDCContent t={t} />
       </InfoPaper>
-      <InfoPaper title={t("about.careerInterest.subtitle")}>
-        <CareerInterestContent t={t} />
+      <InfoPaper title={t("experience.cartovista.subtitle")}>
+        <CartoVistaContent t={t} />
       </InfoPaper>
-      <InfoPaper title={t("about.currentlyLearning.subtitle")}>
-        <LearningContent />
+      <InfoPaper title={t("experience.macadamian.subtitle")}>
+        <MacadamianContent t={t} />
       </InfoPaper>
     </>
   );
